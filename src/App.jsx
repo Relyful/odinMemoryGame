42a86f4a00c7b3dc4831806f52c6d89835c075aa
Relyful <13381270+Relyful@users.noreply.github.com>
2024-10-28 21:30:44 +0100

    Create state for idArray and score, populate empty idArray with random 10 IDs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0);
+  const [idArray, setIdArray] = useState([]);
+  const pokeCount = 1025;
+  
 
+  if (idArray.length < 1) {
+    for (let index = 0; index < 10; index++) {
+      const number = Math.floor(Math.random() * pokeCount);
+      setIdArray(oldArray => [...oldArray, number]);
+    }
+  }
+  
   return (
     <>
-      
+      <h1>Relyful&apos;s Memory Game</h1>
+      <p className="intro">Click each sprite once and only once!</p>
+      {/* cards component here */}
     </>
   )
 }
