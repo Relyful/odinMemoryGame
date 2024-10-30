0f2b0fe1afe004d141cccf1c00f9f3b127cce9f7
Relyful <13381270+Relyful@users.noreply.github.com>
2024-10-30 20:57:04 +0100

    create guessedSprite state variable and handle function to add score and add to sprite array

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [score, setScore] = useState(0);
   const [idArray, setIdArray] = useState([]);
+  const [guessedSprite, setGuessedSprite] = useState([]);
   const pokeCount = 1025;
   
-
-  if (idArray.length < 1) {
+  
+  function idArrayReset() {
     for (let index = 0; index < 10; index++) {
       const number = Math.floor(Math.random() * pokeCount);
-      setIdArray(oldArray => [...oldArray, number]);
+      setIdArray((oldArray) => [...oldArray, number]);
     }
   }
+
+  function addToGuessedSprite(spriteId) {
+    setGuessedSprite(oldArray => [...oldArray, spriteId]);
+  };
+
+  function increaseScore(number) {
+    setScore(score + number);
+  };
   
+
+  //Choose pokemon on fresh load.
+  if (idArray.length < 1) {
+    idArrayReset();
+  };
+
   return (
     <>
       <h1>Relyful&apos;s Memory Game</h1>
       <p className="intro">Click each sprite once and only once!</p>
       {/* cards component here */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
